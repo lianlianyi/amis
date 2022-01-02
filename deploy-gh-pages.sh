80384ef944244bc74f8e22c7deeb1ba5a6592693
导演我躺哪 <lianlianyi@vip.qq.com>
2022-01-02 22:36:23 +0800

    Update deploy-gh-pages.sh

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -10,17 +10,17 @@ npm run build-schemas
 ./node_modules/.bin/fis3 release gh-pages -c
 
 # 拷贝一份兼容之前的访问路径
-cp -r gh-pages/zh-CN/docs/* gh-pages/docs/
+# cp -r gh-pages/zh-CN/docs/* gh-pages/docs/
 
 cp ./schema.json ./gh-pages
 
-cp -r mock gh-pages/
+# cp -r mock gh-pages/
 
-sh build.sh
+# sh build.sh
 
-tar -zcvf sdk.tar.gz sdk
+# tar -zcvf sdk.tar.gz sdk
 
-mv sdk.tar.gz gh-pages/
+# mv sdk.tar.gz gh-pages/
 
 # 加这个 github page 就不会忽略下划线开头的文件
 touch gh-pages/.nojekyll
